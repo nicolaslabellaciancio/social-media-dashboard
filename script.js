3be5950a9c03f3caf5b4e5464d316732223af40b
NICOLAS DANIEL LA BELLA <41810350+nicolaslabellaciancio@users.noreply.github.com>
2020-04-20 20:14:54 -0300

    adding main files

---
+++ script.js
@@ -0,0 +1,22 @@
+const themeSpan = document.querySelector('.slider');
+const themeText = document.querySelector('.theme');
+const body = document.querySelector('body');
+const titleText = Array.from(document.querySelectorAll('.title-text'));
+const followers = Array.from(document.querySelectorAll('.followers'));
+const cards = Array.from(document.querySelectorAll('.box'));
+const texts = Array.from(document.querySelectorAll('.texts'));
+
+function handleClickEvent() {
+    if(themeText.textContent === 'Dark Mode') {
+        themeText.textContent = 'Light Mode';
+    } else {
+        themeText.textContent = 'Dark Mode';
+    }
+    body.classList.toggle('light-bg');
+    titleText.forEach(title => title.classList.toggle('light-text'));
+    followers.forEach(number => number.classList.toggle('light-text'));
+    cards.forEach(card => card.classList.toggle('light-card'));
+    texts.forEach(text => text.style.color = "hsl(228, 12%, 44%)");
+}
+
+themeSpan.addEventListener('click', handleClickEvent);
